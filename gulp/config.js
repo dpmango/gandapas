9d5158d42f957a010d5f04c2aabfbeae04fd85dd
Sergey Khmelevskoy <xs290@me.com>
2017-07-09 23:52:23 +0300

    changed authoprefixer settings

--- gulp/config.js
+++ gulp/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   root: './build',
 
-  autoprefixerConfig: ['last 3 version', '> 1%', 'ie 8', 'ie 9', 'Opera 12.1']
+  autoprefixerConfig: ['last 5 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
 };
